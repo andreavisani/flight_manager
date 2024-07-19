4909712a012b3b183fd3b80fd3e34770bb1d492a
Andrea Visani <visa0004@algonquinlive.com>
2024-07-19 13:05:23 -0400

    Added responsive layout, changed style

--- lib/AddFlight.dart
+++ lib/AddFlight.dart
@@ -3,11 +3,8 @@ import 'package:flight_manager/FlightDAO.dart';
 import 'package:flight_manager/main.dart';
 import 'package:floor/floor.dart';
 import 'package:flutter/material.dart';
-
 import 'package:intl/intl.dart';
-
 import 'Database.dart';
-
 
 class AddFlight extends StatefulWidget {
   @override
@@ -17,16 +14,13 @@ class AddFlight extends StatefulWidget {
 }
 
 class AddFlightState extends State<AddFlight> {
-
   late TextEditingController _controllerDepartureCity;
   late TextEditingController _controllerDestinationCity;
   late TextEditingController _dateTimeControllerDeparture;
   late TextEditingController _dateTimeControllerArrival;
   late FlightDAO flightDAO;
 
-
   Future<void> _selectDateTime(BuildContext context, TextEditingController controller) async {
-
     DateTime? pickedDate = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
@@ -55,24 +49,18 @@ class AddFlightState extends State<AddFlight> {
     }
   }
 
-  void addFlight(){
+  void addFlight() {
     setState(() {
       final DateTime departureTime = DateFormat('yyyy-MM-dd HH:mm').parse(_dateTimeControllerDeparture.text);
       final DateTime arrivalTime = DateFormat('yyyy-MM-dd HH:mm').parse(_dateTimeControllerArrival.text);
 
-      // putting ID++, make sure the id is increased every time
       var newFlight = Flight(Flight.ID++, _controllerDepartureCity.value.text, _controllerDestinationCity.value.text, departureTime, arrivalTime);
 
-      //add to db
       flightDAO.insertFlight(newFlight);
 
-
+      Navigator.pushNamed(context, '/flights');
     });
   }
-
-
-
-
 
   @override
   void initState() {
@@ -85,16 +73,17 @@ class AddFlightState extends State<AddFlight> {
 
     $FloorFlightManagerDatabase.databaseBuilder('app_database.db').build().then((database) {
       flightDAO = database.GetDao;
-
     });
-
   }
 
   @override
   void dispose() {
     super.dispose();
+    _controllerDepartureCity.dispose();
+    _controllerDestinationCity.dispose();
+    _dateTimeControllerDeparture.dispose();
+    _dateTimeControllerArrival.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -103,64 +92,90 @@ class AddFlightState extends State<AddFlight> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("Add new flight"),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
-            // === DEPARTURE CITY ===
+            Text(
+              "Add New Flight",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blueAccent),
+            ),
+            SizedBox(height: 16),
             TextField(
               controller: _controllerDepartureCity,
               decoration: InputDecoration(
                 hintText: "Departure City",
                 border: OutlineInputBorder(),
                 labelText: "Departure City",
+                prefixIcon: Icon(Icons.flight_takeoff, color: Colors.blueAccent),
               ),
             ),
-
-            // === DESTINATION CITY ===
+            SizedBox(height: 16),
             TextField(
               controller: _controllerDestinationCity,
               decoration: InputDecoration(
                 hintText: "Destination City",
                 border: OutlineInputBorder(),
                 labelText: "Destination City",
+                prefixIcon: Icon(Icons.flight_land, color: Colors.blueAccent),
               ),
             ),
-
-            // === DEPARTURE TIME ===
+            SizedBox(height: 16),
             TextField(
               controller: _dateTimeControllerDeparture,
               readOnly: true,
               decoration: InputDecoration(
-                hintText: 'Select Date and Time',
-                suffixIcon: Icon(Icons.calendar_today),
+                hintText: 'Select Departure Date and Time',
+                border: OutlineInputBorder(),
+                labelText: "Departure Time",
+                prefixIcon: Icon(Icons.calendar_today, color: Colors.blueAccent),
               ),
               onTap: () => _selectDateTime(context, _dateTimeControllerDeparture),
             ),
-
-            // === ARRIVAL TIME ===
+            SizedBox(height: 16),
             TextField(
               controller: _dateTimeControllerArrival,
               readOnly: true,
               decoration: InputDecoration(
-                hintText: 'Select Date and Time',
-                suffixIcon: Icon(Icons.calendar_today),
+                hintText: 'Select Arrival Date and Time',
+                border: OutlineInputBorder(),
+                labelText: "Arrival Time",
+                prefixIcon: Icon(Icons.calendar_today, color: Colors.blueAccent),
               ),
               onTap: () => _selectDateTime(context, _dateTimeControllerArrival),
             ),
+            SizedBox(height: 24),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children:[
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/flights');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent, // background color
+                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                      textStyle: TextStyle(fontSize: 18),
+                    ),
+                    child: Text("Go Back", style: TextStyle(color: Colors.white),),
+                  ),
+                  ElevatedButton(
+                  onPressed: addFlight,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green, // background color
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                    textStyle: TextStyle(fontSize: 18),
+                  ),
+                  child: Text("ADD FLIGHT", style: TextStyle(color: Colors.white),),
 
-            ElevatedButton(onPressed: addFlight, child: Text("ADD")),
-
-
-
-
+                ),
+              ]
+            ),
           ],
         ),
       ),
     );
   }
-
-
 }
--- lib/FlightsPage.dart
+++ lib/FlightsPage.dart
@@ -1,7 +1,9 @@
 import 'package:flight_manager/Flight.dart';
 import 'package:flight_manager/FlightDAO.dart';
 import 'package:flight_manager/main.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'Database.dart';
 
@@ -18,6 +20,8 @@ class PersonalInfoState extends State<FlightsPage> {
   var flights = <Flight>[];
 
   late FlightDAO flightDAO;
+
+  Flight? selectedItem = null;
 
   @override
   void initState() {
@@ -41,6 +45,220 @@ class PersonalInfoState extends State<FlightsPage> {
     super.dispose();
   }
 
+
+  Widget FlightList() {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Flight List',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blueAccent),
+          ),
+          SizedBox(height: 16),
+          Expanded(
+            child: ListView.builder(
+              itemCount: flights.length,
+              itemBuilder: (BuildContext context, int rowNum) {
+                return GestureDetector(
+                  onLongPress: () {
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Remove Flight'),
+                        content: const Text('Are you sure you want to remove this flight?'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              removeFlight(rowNum);
+                              Navigator.pop(context, 'Yes');
+                            },
+                            child: Text("Yes", style: TextStyle(color: Colors.red)),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'No'),
+                            child: const Text('No'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  onTap: () {
+                    setState(() {
+                      selectedItem = flights[rowNum]; // selectedItem no longer null
+                    });
+                  },
+                  child: Card(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                    elevation: 3,
+                    child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Departure: ${flights[rowNum].departureCity}",
+                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "Destination: ${flights[rowNum].destinationCity}",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                          Icon(Icons.airplanemode_active, color: Colors.blueAccent),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/addFlight');
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blueAccent, // background color
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              textStyle: TextStyle(fontSize: 18),
+            ),
+            child: Text("Add Flight", style: TextStyle(color: Colors.white),),
+          ),
+          SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
+
+
+  Widget DetailsPage() {
+    if (selectedItem == null) {
+      return Center(
+        child: Text(
+          "Select a Flight to see the details",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blueAccent),
+        ),
+      );
+    } else {
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Flight Details",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blueAccent),
+            ),
+            SizedBox(height: 16),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              elevation: 3,
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "ID: ${selectedItem!.id}",
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      "Departure: ${selectedItem!.departureCity}",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      "Destination: ${selectedItem!.destinationCity}",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      "Departure Time: ${selectedItem!.departureTime}",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      "Arrival Time: ${selectedItem!.arrivalTime}",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedItem = null;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    textStyle: TextStyle(fontSize: 18),
+                  ),
+                  child: Text("Go Back", style: TextStyle(color: Colors.white),),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    textStyle: TextStyle(fontSize: 18),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      flightDAO.deleteFlight(selectedItem!);
+                      flights.remove(selectedItem!);
+                      selectedItem = null;
+                    });
+                  },
+                  child: Text("DELETE", style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  Widget responsiveLayout(){
+    var size = MediaQuery.of(context).size;
+    var height = size.height;
+    var width = size.width;
+
+    if( (width>height) && (width > 720)) { // if in Landscape mode
+      return Row( children:[
+        Expanded( flex: 2, child: FlightList()),
+        Expanded( flex: 3, child: DetailsPage())] //Right side
+
+
+      );
+    } else {// if Portrait mode
+      if (selectedItem == null){
+        return FlightList();
+      } else {
+        return DetailsPage();
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,53 +266,7 @@ class PersonalInfoState extends State<FlightsPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("Flights"),
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            Expanded(
-              child: ListView.builder(
-                itemCount: flights.length,
-                itemBuilder: (BuildContext context, int rowNum) {
-                  return GestureDetector(
-                    onLongPress: () {
-                      showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: const Text('Remove'),
-                          content: const Text('Remove instance?'),
-                          actions: <Widget>[
-                            ElevatedButton(
-                              onPressed: () {
-                                removeFlight(rowNum);
-                              },
-                              child: Text("Yes"),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text("Departure: ${flights[rowNum].departureCity}"),
-                        Text("Destination: ${flights[rowNum].destinationCity}"),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-
-            // === ADD BUTTON ===
-            ElevatedButton(onPressed: () { Navigator.pushNamed(context, '/addFlight');} , child: Text("Add flight"),),
-
-
-          ],
-        ),
-      ),
+      body: responsiveLayout()
     );
   }
 
