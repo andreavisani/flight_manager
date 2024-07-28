7eee64fa3ac663f9b1a99b9ff28d59a4823f82f2
DikchhaR <rijaldikchha@gmail.com>
2024-07-28 17:59:48 -0400

    Established a route in the main dart

--- lib/CustomerDAO.dart
+++ lib/CustomerDAO.dart
@@ -10,4 +10,15 @@ abstract class CustomerDAO{
   @Query('SELECT * FROM Customer') // ToDoItem is the entity object
   Future<List<Customer>> selectEverything(); // returns an arrayList of ToDoItems
 
+@insert //make it an insert function to generate
+Future<int> insertCustomer(Customer customer);
+
+  @delete  // generate the deletion statement in code
+  Future<void> deleteCustomer(Customer customer);
+
+  @update
+  Future<void> updateCustomer(Customer customer);
+
 }
+
+
